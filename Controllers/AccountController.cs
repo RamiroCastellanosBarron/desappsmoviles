8986eafea77ae62385258dfa9d1189972628e5f1
Ramiro Castellanos Barrón <66740266+RamiroCastellanosBarron@users.noreply.github.com>
2022-11-25 03:08:56 -0600

    account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace movies.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<LoginResponse> Login([FromQuery]User user)
+        {
+            if(user.Username == "Ramiro" && user.Password == "Castellanos") return Ok(new LoginResponse { Code = "Successfull" });
+            
+            return Ok(new LoginResponse { Code = "Error"});
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string Code { get; set; }
+    }
+}
